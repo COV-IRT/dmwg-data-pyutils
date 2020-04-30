c46737e052ad33d200332e80d7cfb489b100eae2
Kyle Hernandez <kmhernan84@gmail.com>
2020-04-30 12:50:48 -0500

    feat(tests): add cleanup func

--- tests/utils.py
+++ tests/utils.py
@@ -19,3 +19,22 @@ def captured_output():
         yield sys.stdout, sys.stderr
     finally:
         sys.stdout, sys.stderr = old_out, old_err
+
+
+def cleanup_files(files):
+    """
+    Takes a file or a list of files and removes them.
+    """
+
+    def _do_remove(fil):
+        if os.path.exists(fil):
+            os.remove(fil)
+
+    flist = []
+    if isinstance(files, list):
+        flist = files[:]
+    else:
+        flist = [files]
+
+    for fil in flist:
+        _do_remove(fil)
