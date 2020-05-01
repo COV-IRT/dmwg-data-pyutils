d375879cfd0789747e3e594a966b5a054477faa7
Kyle Hernandez <kmhernan84@gmail.com>
2020-05-01 15:19:24 -0500

    chore(tests): _collect_mutations

--- tests/test_common_nextstrain.py
+++ tests/test_common_nextstrain.py
@@ -11,6 +11,33 @@ from utils import captured_output, cleanup_files
 class TestNextStrainParser(unittest.TestCase):
     to_remove = []
 
+    def get_basic_node(self, name):
+        """Utility function to generate basic node template"""
+        curr = {"name": name, "node_attrs": {}, "branch_attrs": {}, "children": []}
+        return curr
+
+    def build_test_tree(self, n_nodes=10):
+        tot = 0
+        root = self.get_basic_node("root")
+
+        left = self.get_basic_node("left0")
+        left["branch_attrs"]["mutations"] = {"S": ["A"]}
+
+        left_1 = self.get_basic_node("left1")
+        del left_1["children"]
+        left["children"].append(left_1)
+
+        left_2 = self.get_basic_node("left2")
+        left_2["branch_attrs"]["mutations"] = {"nuc": ["B"]}
+        left_3 = self.get_basic_node("left3")
+        del left_3["children"]
+        left_2["children"].append(left_3)
+        left["children"].append(left_2)
+        root["children"].append(left)
+
+        tree = {"tree": root}
+        return tree
+
     def test_from_file_path_assert(self):
         tobj = {"_tree": 1}
         (fd, fn) = tempfile.mkstemp()
@@ -33,51 +60,67 @@ class TestNextStrainParser(unittest.TestCase):
         self.assertEqual(res.obj, tobj)
 
     def test_get_value(self):
-        dat = {'tree': {'value': 0}}
+        dat = {"tree": {"value": 0}}
         obj = NextStrainParser(dat)
-        res = obj.get_value(dat['tree'])
+        res = obj.get_value(dat["tree"])
         self.assertEqual(res, 0)
 
-        dat = {'tree': 0}
-        res = obj.get_value(dat['tree'])
+        dat = {"tree": 0}
+        res = obj.get_value(dat["tree"])
         self.assertEqual(res, 0)
 
     def test_parse_attrs(self):
-        dat = {'tree': {'A': {'value': 0}, 'B': 0}}
+        dat = {"tree": {"A": {"value": 0}, "B": 0}}
         obj = NextStrainParser(dat)
-        res = obj.parse_attrs(dat['tree'], ['A'])
-        self.assertEqual(res, {'A': 0}) 
+        res = obj.parse_attrs(dat["tree"], ["A"])
+        self.assertEqual(res, {"A": 0})
 
-        res = obj.parse_attrs(dat['tree'], ['A', 'B'])
-        self.assertEqual(res, {'A': 0, 'B': 0}) 
+        res = obj.parse_attrs(dat["tree"], ["A", "B"])
+        self.assertEqual(res, {"A": 0, "B": 0})
 
-        res = obj.parse_attrs(dat['tree'], ['A', 'B', 'C'])
-        self.assertEqual(res, {'A': 0, 'B': 0, 'C': None}) 
+        res = obj.parse_attrs(dat["tree"], ["A", "B", "C"])
+        self.assertEqual(res, {"A": 0, "B": 0, "C": None})
 
     def test_append_parents(self):
-        dat = {'tree': {'name': "node0", "children": [{"name": "node1"}]}}
+        dat = {"tree": {"name": "node0", "children": [{"name": "node1"}]}}
         obj = NextStrainParser(dat)
         obj._append_parents()
-        self.assertEqual(obj.root["parent"]['name'], obj.root["name"])
-        self.assertEqual(obj.root['name'], obj.root["children"][0]['parent']['name'])
+        self.assertEqual(obj.root["parent"]["name"], obj.root["name"])
+        self.assertEqual(obj.root["name"], obj.root["children"][0]["parent"]["name"])
 
     def test__flatten_nodes(self):
-        dat = {'tree': {'name': "node0", "children": [{"name": "node1"}]}}
+        dat = {"tree": {"name": "node0", "children": [{"name": "node1"}]}}
         obj = NextStrainParser(dat)
         obj._append_parents()
         res = obj._flatten_nodes()
         self.assertEqual(len(res), 2)
-        self.assertEqual(res[0]['name'], 'node0')
-        self.assertEqual(res[1]['name'], 'node1')
+        self.assertEqual(res[0]["name"], "node0")
+        self.assertEqual(res[1]["name"], "node1")
 
-    def test__mut_reduc(self):
-        obj = NextStrainParser({'tree': None})
-        dat = [
-            {'A': [1, 2]},
-            {'A': [1, 2, 3], 'B': [1]}
-        ]
+    def test__mut_reduce(self):
+        obj = NextStrainParser({"tree": None})
+        dat = [{"A": [1, 2]}, {"A": [1, 2, 3], "B": [1]}]
         res = obj._mut_reduce(dat)
-        self.assertEqual(res, {'A': [1, 2, 3], 'B': [1]}) 
+        self.assertEqual(res, {"A": [1, 2, 3], "B": [1]})
+
+    def test__collect_mutations(self):
+        dat = self.build_test_tree()
+        exp = {
+            "root": {},
+            "left0": {"S": ["A"]},
+            "left2": {"nuc": ["B"], "S": ["A"]},
+            "left3": {"nuc": ["B"], "S": ["A"]},
+            "left1": {"S": ["A"]},
+        }
+        obj = NextStrainParser(dat)
+        obj._append_parents()
+        mdict = {}
+        for node in obj._flatten_nodes():
+            key = node["name"]
+            self.assertTrue(key not in mdict)
+            mdict[key] = obj._collect_mutations(node)
+
+        self.assertEqual(mdict, exp)
 
     def tearDown(self):
         cleanup_files(TestNextStrainParser.to_remove)
